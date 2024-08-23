4a895f63177e16db69646fc68f75aa8930e54515
RAJAT-K26 <97009707+RAJAT-K26@users.noreply.github.com>
2024-08-24 01:58:26 +0530

    UI enhanced

--- tokengen/src/App.js
+++ tokengen/src/App.js
@@ -10,26 +10,35 @@ import Container from '@mui/material/Container';
 import { useState } from 'react';
 // import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import { createTheme } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+import { shadows } from '@mui/system';
+
 
 
 
 
 function App() {
 
-  
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: purple[500],
+      }
+    }
+  });
 
-  const [blueTokens, setBlueTokens] = useState(0); 
+
+
+  const [blueTokens, setBlueTokens] = useState();
   const [bluePrefix, setBluePrefix] = useState('');
   const [bluePerRow, setBluePerRow] = useState(1);
-  const [redTokens, setRedTokens] = useState(0);
+  const [redTokens, setRedTokens] = useState();
   const [redPrefix, setRedPrefix] = useState('');
   const [redPerRow, setRedPerRow] = useState(1);
   const [generatedTokens, setGeneratedTokens] = useState([]); //stores generated tokens
@@ -50,153 +59,223 @@ function App() {
   const handleClear = () => {
     setBluePerRow(1);
     setBluePrefix('');
-    setBlueTokens(0);
+    setBlueTokens();
     setRedPerRow(1);
     setRedPrefix('');
-    setRedTokens(0);
+    setRedTokens();
     setGeneratedTokens([]);
   };
 
+
+  const handleSliderChange = (event, newValue) => {
+    setBlueTokens(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value === '' ? '' : Number(event.target.value);
+    setBlueTokens(value);
+  };
   return (
     <>
-     
-      
-     <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Number of blue tokens"
-          type="number"
-          onChange={(e) => setBlueTokens(Number(e.target.value))}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-        <h1>{blueTokens}</h1>
-      </Grid>
-      <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Blue token prefix"
-          type="text"
-          onChange={(e) => setBluePrefix(e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-      </Grid>
-      <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Blue token per row"
-          type="number"
-          onChange={(e) => setBluePerRow(Number(e.target.value))}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-      </Grid>
-      <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Number of red tokens"
-          type="number"
-          onChange={(e) => setRedTokens(Number(e.target.value))}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-      </Grid>
-      <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Red token prefix"
-          type="text"
-          onChange={(e) => setRedPrefix(e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-      </Grid>
-      <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField
-          id="outlined-number"
-          label="Red token per row"
-          type="number"
-          onChange={(e) => setRedPerRow(Number(e.target.value))}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ paddingRight: '8px', marginTop: '20px' }}
-        />
-      </Grid>
-      <Button variant="outlined" onClick={handleGenerated}>
-        Generate
-      </Button>
-      <Button variant="outlined" onClick={handleClear}>
-        Clear
-      </Button>
+      <div style={{ backgroundColor: '#e8eaf6', height: '100vw' }}>
+        <Container maxWidth='md' sx={{ borderRadius: '3vh', marginTop: '2px' }} >
 
 
-{/* rendering boxes / tokens */}
+
+          <Container maxWidth="sm" sx={{ backgroundColor: '#dcdff2', borderRadius: '3vh', paddingBottom: '3vh', boxShadow: 6}}>
+            <Grid xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="h2" gutterBottom sx={{ color: "#35373f" }}>
+                Token Generator
+              </Typography>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Number of blue tokens"
+                    type="number"
+                    value={blueTokens}
+                    onChange={(e) => setBlueTokens(Number(e.target.value))}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    sx={{ paddingRight: '8px', marginTop: '20px' }}
+                  />
 
 
-      <div>
-        <Grid container spacing={2} style={{ marginTop: '20px' }}>
-          {generatedTokens
-          .filter((token) => token.color === 'blue')
-            .map((token, index) => (
-              <Grid item xs={12 / bluePerRow} key={index}>
-                <Box
-                  sx={{
-                    backgroundColor: 'blue',
-                    color: 'white',
-                    width: '100px',
-                    height: '100px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: '5px',
-                  }}
-                >
-                  <h3>{token.label}</h3>
-                </Box>
+                </Grid>
+
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Blue token prefix"
+                    type="text"
+                    value={bluePrefix}
+                    onChange={(e) => setBluePrefix(e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    sx={{ paddingRight: '8px', marginTop: '20px' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <Container>
+                    blue per row<Slider
+                      color=''
+                      size='small'
+                      valueLabelDisplay="auto"
+                      shiftStep={30}
+                      step={1}
+                      onChange={(e) => setBluePerRow(Number(e.target.value))}
+                      marks
+                      min={1}
+                      max={10}
+                    />
+                  </Container>
+                </Grid>
               </Grid>
-            ))}
-
-            {generatedTokens.some((token) => token.color === "red") &&
-            (
-              <Grid xs={12}>
-                <hr style={{borderColor: "Transparent"}} />
+              <Grid item xs={6}>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Number of red tokens"
+                    type="number"
+                    value={redTokens}
+                    onChange={(e) => setRedTokens(Number(e.target.value))}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    sx={{ paddingRight: '8px', marginTop: '20px' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Red token prefix"
+                    type="text"
+                    value={redPrefix}
+                    onChange={(e) => setRedPrefix(e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    sx={{ paddingRight: '8px', marginTop: '20px' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <Container>
+                    Red per row<Slider
+                    color=''
+                      aria-label="Temperature"
+                      defaultValue={0}
+                      size='small'
+                      valueLabelDisplay="auto"
+                      shiftStep={30}
+                      step={1}
+                      onChange={(e) => setRedPerRow(Number(e.target.value))}
+                      marks
+                      min={1}
+                      max={10}
+                    />
+                  </Container>
+                </Grid>
               </Grid>
-            )}
 
-            
-          {generatedTokens
-            .filter((token) => token.color === 'red')
-            .map((token, index) => (
-              <Grid item xs={12 / redPerRow} key={index}>
-                <Box
-                  sx={{
-                    backgroundColor: 'red',
-                    color: 'white',
-                    width: '100px',
-                    height: '100px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: '5px',
-                  }}
-                >
-                  <h3>{token.label}</h3>
-                </Box>
-              </Grid>
-            ))}
-        </Grid>
+            </Grid>
+          </Container>
+
+
+
+
+
+          <Container sx={{
+            marginTop: "1vw", display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <ButtonGroup variant="text" aria-label="Basic button group" >
+              <Button variant='contained' sx={{backgroundColor:'#b3bbe4', color:'#363844',boxShadow: 7}} onClick={handleGenerated}>
+                Generate
+              </Button>
+              <Button variant='contained' sx={{color:'white', backgroundColor:'#b3bbe4',boxShadow: 7}} onClick={handleClear}>
+                Clear
+              </Button>
+
+            </ButtonGroup>
+          </Container>
+
+
+
+          {/* rendering boxes / tokens */}
+
+
+          <div style={{ marginTop: "1vw" }}>
+            <Grid
+              container
+              spacing={2}
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center', // Center text within the boxes
+              }}
+            >
+              {generatedTokens
+                .filter((token) => token.color === 'blue')
+                .map((token, index) => (
+                  <Grid item xs={12 / bluePerRow} key={index}>
+                    <Box
+                      sx={{
+                        boxShadow: 8,
+                        backgroundColor: '#5f6dff',
+                        color: 'white',
+                        width: '70px',
+                        height: '70px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        // margin: '0 auto', // Center the box within the grid item
+                        borderRadius: '5px',
+                      }}
+                    >
+                      <h4>{token.label}</h4>
+                    </Box>
+                  </Grid>
+                ))}
+
+              {generatedTokens.some((token) => token.color === 'red') && (
+                <Grid item xs={12}>
+                  <hr style={{ borderColor: 'transparent' }} />
+                </Grid>
+              )}
+
+              {generatedTokens
+                .filter((token) => token.color === 'red')
+                .map((token, index) => (
+                  <Grid item xs={12 / redPerRow} key={index}>
+                    <Box
+                      sx={{
+                        boxShadow: 8,
+                        backgroundColor: '#f66263',
+                        color: 'white',
+                        width: '70px',
+                        height: '70px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        // margin: '0 auto', // Center the box within the grid item
+                        borderRadius: '5px',
+                      }}
+                    >
+                      <h4>{token.label}</h4>
+                    </Box>
+                  </Grid>
+                ))}
+            </Grid>
+
+
+          </div>
+        </Container>
       </div>
     </>
   );
