f5c7055131d9e42c6441ce5d502a94c4f1f2ca9b
RAJAT-K26 <97009707+RAJAT-K26@users.noreply.github.com>
2024-08-23 23:36:02 +0530

    rendering of tokens / boxes (raw)

--- tokengen/src/App.js
+++ tokengen/src/App.js
@@ -24,16 +24,38 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function App() {
 
-  const handleGenerated = () => {}
-  const handleClear = () => {}
+  
 
-  const [blueTokens, setBlueTokens] = useState(0);
+  const [blueTokens, setBlueTokens] = useState(0); 
   const [bluePrefix, setBluePrefix] = useState('');
   const [bluePerRow, setBluePerRow] = useState(1);
   const [redTokens, setRedTokens] = useState(0);
   const [redPrefix, setRedPrefix] = useState('');
   const [redPerRow, setRedPerRow] = useState(1);
   const [generatedTokens, setGeneratedTokens] = useState([]); //stores generated tokens
+
+  const handleGenerated = () => {
+    const newToken = [];
+    for (let i = 0; i < blueTokens; i++) {
+      newToken.push({ color: 'blue', label: `${bluePrefix}${i + 1}` });
+    }
+
+    for (let i = 0; i < redTokens; i++) {
+      newToken.push({ color: 'red', label: `${redPrefix}${i + 1}` });
+    }
+
+    setGeneratedTokens(newToken); // pushes the newToken generatedTokens array
+  };
+
+  const handleClear = () => {
+    setBluePerRow(1);
+    setBluePrefix('');
+    setBlueTokens(0);
+    setRedPerRow(1);
+    setRedPrefix('');
+    setRedTokens(0);
+    setGeneratedTokens([]);
+  };
 
   return (
     <>
@@ -118,6 +140,56 @@ function App() {
       <Button variant="outlined" onClick={handleClear}>
         Clear
       </Button>
+
+
+{/* rendering boxes / tokens */}
+
+
+      <div>
+        <Grid container spacing={2} style={{ marginTop: '20px' }}>
+          {generatedTokens.filter((token) => token.color === 'blue')
+            .map((token, index) => (
+              <Grid item xs={12 / bluePerRow} key={index}>
+                <Box
+                  sx={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    width: '100px',
+                    height: '100px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: '5px',
+                  }}
+                >
+                  <h3>{token.label}</h3>
+                </Box>
+              </Grid>
+            ))}
+
+            
+          {generatedTokens
+            .filter((token) => token.color === 'red')
+            .map((token, index) => (
+              <Grid item xs={8 / redPerRow} key={index}>
+                <Box
+                  sx={{
+                    backgroundColor: 'red',
+                    color: 'white',
+                    width: '100px',
+                    height: '100px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: '5px',
+                  }}
+                >
+                  <h3>{token.label}</h3>
+                </Box>
+              </Grid>
+            ))}
+        </Grid>
+      </div>
     </>
   );
 }
